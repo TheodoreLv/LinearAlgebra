a8b7baaf87b5d36f2991a8c61a39321ff396b867
Theodore.L <32017870+TheodoreLv@users.noreply.github.com>
2021-03-10 17:40:35 +0800

    添加点乘运算

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from playLA.Vector import Vector
 
+
 if __name__ == '__main__':
     vec = Vector([5, 2])
     print(vec)
-    print('vec维度：', len(vec))
+    print('vec length：', len(vec))
     print('vec[0] = {}, vec[1] = {}'.format(vec[0], vec[1]))
 
     vec2 = Vector([3, 1])
@@ -19,3 +20,16 @@ if __name__ == '__main__':
     zero2 = Vector.zero(2)
     print(zero2)
     print('{} + {} = {}'.format(vec, zero2, vec + zero2))
+
+    print('norm({}) = {}'.format(vec, vec.norm()))
+    print('norm({}) = {}'.format(vec2, vec2.norm()))
+
+    print('normalize {} is {}'.format(vec, vec.normalize()))
+    print(vec.normalize().norm())
+
+    try:
+        zero2.normalize()
+    except ZeroDivisionError:
+        print('Cannot normalize zero vector {}'.format(zero2))
+
+    print(vec.dot(vec2))
--- playLA/Vector.py
+++ playLA/Vector.py
@@ -1,3 +1,8 @@
+import math
+
+from ._global import EPSILON
+
+
 class Vector(object):
 
     def __init__(self, lst):
@@ -7,6 +12,16 @@ class Vector(object):
     def zero(cls, dim):
         """返回dim维零向量"""
         return cls([0] * dim)
+
+    def norm(self):
+        """返回向量的模"""
+        return math.sqrt(sum(e**2 for e in self))
+
+    def normalize(self):
+        """返回单位向量"""
+        if self.norm() < EPSILON:
+            raise ZeroDivisionError("Normalize error! norm is zero.")
+        return Vector(self._values) / self.norm()
 
     def __add__(self, other):
         """向量加法"""
@@ -18,6 +33,11 @@ class Vector(object):
         assert len(self) == len(other), "Error in subtracting, Length of vectors must be same."
         return Vector([a - b for a, b in zip(self, other)])
 
+    def dot(self, other):
+        """返回向量的点乘（结果为标量）"""
+        assert len(self) == len(other), "Error in dot product. Length of vectors mut be same."
+        return sum(a * b for a, b in zip(self, other))
+
     def __mul__(self, k):
         """向量的数量乘法(self * k)"""
         return Vector([k * e for e in self])
@@ -26,11 +46,15 @@ class Vector(object):
         """向量的数量乘法(k * self)"""
         return self * k
 
+    def __truediv__(self, k):
+        """返回向量的数量除法(self / k)"""
+        return 1 / k * self
+
     def __pos__(self):
-        return self * 1
+        return 1 * self
 
     def __neg__(self):
-        return self * -1
+        return -1 * self
 
     def __iter__(self):
         return self._values.__iter__()
