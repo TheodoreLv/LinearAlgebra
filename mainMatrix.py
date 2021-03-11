cc353913ac7dc49f60ffb9b6179f9b6e62b60a4c
Theodore.L <1145281480@qq.com>
2021-03-11 11:37:57 +0800

    add scalar-mul method  to Matrix

--- mainMatrix.py
+++ mainMatrix.py
@@ -9,3 +9,10 @@ if __name__ == '__main__':
     print('matrix[0, 0] = {}'.format(matrix[0, 0]))
     print('matrix.row_vector(1) = {}'.format(matrix.row_vector(1)))
     print('matrix.col_vector(1) = {}'.format(matrix.col_vector(1)))
+
+    matrix2 = Matrix([[5, 6], [7, 8]])
+    print('add: {}'.format(matrix + matrix2))
+    print('sub: {}'.format(matrix - matrix2))
+    print('scalar-mul: {}'.format(matrix * 2))
+    print('scalar-mul: {}'.format(2 * matrix))
+    print('scalar-div: {}'.format(matrix / 2))
--- playLA/Matrix.py
+++ playLA/Matrix.py
@@ -6,6 +6,10 @@ class Matrix(object):
     def __init__(self, list2d):
         self._values = [row[:] for row in list2d]
 
+    @classmethod
+    def zero(cls, r, c):
+        return cls([[0] * c for _ in range(r)])
+
     def row_vector(self, index):
         """返回矩阵的第index个行向量"""
         return Vector(self._values[index])
@@ -36,6 +40,36 @@ class Matrix(object):
     def shape(self):
         """返回矩阵形状"""
         return len(self._values), len(self._values[0])
+
+    def __add__(self, other):
+        """返回两个矩阵加法"""
+        assert self.shape() == other.shape(), 'Error in adding, Shape must be same.'
+        return Matrix([[a + b for a, b in zip(self.row_vector(i), other.row_vector(i))] for i in range(self.row_num())])
+
+    def __sub__(self, other):
+        """返回两个矩阵加法"""
+        assert self.shape() == other.shape(), 'Error in subtracting, Shape must be same.'
+        return Matrix([[a - b for a, b in zip(self.row_vector(i), other.row_vector(i))] for i in range(self.row_num())])
+
+    def __mul__(self, other):
+        """返回矩阵的数量乘法self*other"""
+        return Matrix([[e * other for e in self.row_vector(i)] for i in range(self.row_num())])
+
+    def __rmul__(self, other):
+        """返回矩阵的数量乘法other*self"""
+        return self * other
+
+    def __truediv__(self, other):
+        """返回矩阵的数量除法self/other"""
+        return Matrix([[e / other for e in self.row_vector(i)] for i in range(self.row_num())])
+
+    def __pos__(self):
+        """返回矩阵取正的结果"""
+        return 1 * self
+
+    def __neg__(self):
+        """返回矩阵取负的结果"""
+        return -1 * self
 
     def __repr__(self):
         return 'Matrix({})'.format(self._values)
